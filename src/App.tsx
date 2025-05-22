aa272e0dd0d8aacb2fe8a0811d7d5399911696b1
Meet kasediya <meetkasediya2004@gmail.com>
2025-05-22 16:13:16 +0530

    Add interactive cards component with remove functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import Hero from './components/Hero'
 import Features from './components/Features'
 import About from './components/About'
 import Footer from './components/Footer'
-
+import Cards from './components/Cards'
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <Features />
+      <Cards />
       <About />
       <Footer />
     </>
